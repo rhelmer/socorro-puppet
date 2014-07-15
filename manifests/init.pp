ed4f7b4d9f9c75a4112d65ce34161ec5739ad877
Robert Helmer <rhelmer@rhelmer.org>
2014-07-14 17:17:29 -0700

    configure collector to use S3

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,8 @@ $crash_stats_url    = hiera('crash_stats_url')
 $crash_stats_host   = hiera('crash_stats_host')
 $postgresql_url     = hiera('postgresql_url')
 $default_product    = hiera('default_product')
+$aws_access_key     = hiera('aws_access_key')
+$aws_secret_key     = hiera('aws_secret_key')
 
 node default {
   include webapp::socorro
